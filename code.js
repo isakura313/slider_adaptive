a5a52a4dbe0bfe514cb888d1150bb5cbfdf9da8a
Paul <isakura313@gmail.com>
2019-11-05 11:37:27 +0300

    almost without jquery

--- code.js
+++ code.js
@@ -2,97 +2,95 @@
 //сделать так, что бы при загрузке отображался первый кружочек
 //сделать общий рефакторинг кода
 //мы удаляем jquery из проекта и переписываем на новый ES6
+//починить баг при клике направо
 
 
 let slider = document.querySelector('.slider'); //получение общего слайдера
 let sliderLenta = slider.querySelector('.slider-lenta'); // общая лента
 let sliderItems = slider.querySelectorAll('.slider-lenta-item.real'); // получаем настоящие элементы
-// sliderLenta.css('width', (sliderItems.length+2) * 100 +"%"); // отрисовка всей ширины
-sliderLenta.style.width = (sliderItems.length+2)*100 + "%"
 let sliderBullets = document.querySelectorAll('.slider-bullets div'); // здесь у нас буллетсы
 let arr_left = slider.querySelector('.slider-arrow-left');
-//название функции на английском и описывает некое действие
-
-
+let arr_right = slider.querySelector('.slider-arrow-right');
+sliderLenta.style.width = (sliderItems.length + 2) * 100 + "%"
 
 let nowShowIndex = 0; // наш изначальный индекс
 bullets_draw(nowShowIndex); // изначальная отрисовка
 
-arr_left.onclick = function(){
-    if(sliderLenta.classList.contains('animated')){
+arr_left.onclick = function () {
+    if (sliderLenta.classList.contains('animated')) {
         return;
     }
 
     sliderLenta.classList.add('animated');
-    if(nowShowIndex <= 0){
+    if (nowShowIndex <= 0) {
         nowShowIndex = nowShowIndex - 1;
-        
+
         $(sliderLenta).animate({
             left: -100 * (nowShowIndex + 1) + "%"
-        }, 600, function(){
+        }, 600, function () {
 
             nowShowIndex = sliderItems.length - 1;
-            sliderLenta.style.left = -100 * (nowShowIndex + 1) +"%";
+            sliderLenta.style.left = -100 * (nowShowIndex + 1) + "%";
             sliderLenta.classList.remove('animated');
             bullets_draw(nowShowIndex);
         });
-    } else{
+    } else {
         nowShowIndex = nowShowIndex - 1;
         $(sliderLenta).animate({
             left: -100 * (nowShowIndex + 1) + "%"
-            }, 600, function(){
+        }, 600, function () {
             sliderLenta.classList.remove('animated');
-            })
-        }
-        bullets_draw(nowShowIndex);
+        })
+    }
+    bullets_draw(nowShowIndex);
 };
 
 
-slider.querySelector('.slider-arrow-right').onclick = function(){
+arr_right.onclick = function () {
 
-    sliderLenta.addClass('animated');
-    if(nowShowIndex >= sliderItems.length - 1){
+    sliderLenta.classList.add('animated');
+    if (nowShowIndex >= sliderItems.length - 1) {
         nowShowIndex = nowShowIndex + 1;
-        
-        sliderLenta.animate({
+        $(sliderLenta).animate({
             left: -100 * (nowShowIndex + 1) + "%"
-        }, 600, function(){
+
+        }, 600, function () {
             nowShowIndex = 0;
-            sliderLenta.css('left', -100 * (nowShowIndex + 1) +"%");
-            sliderLenta.removeClass('animated');
-        bullets_draw(nowShowIndex);
+
+            sliderLenta.style.left = 'left', -100 * (nowShowIndex + 1) + "%";
+
+            sliderLenta.classList.remove('animated');
+            bullets_draw(nowShowIndex);
         });
-        if(sliderLenta.hasClass('animated')){
+        if (sliderLenta.classList.contains('animated')) {
             return;
         }
-
-     } else{
-            nowShowIndex = nowShowIndex + 1;
-            sliderLenta.animate({
-                left: -100 * (nowShowIndex + 1) + "%"
-            }, 600, function(){
-                sliderLenta.removeClass('animated');
+    } else {
+        nowShowIndex = nowShowIndex + 1;
+        $(sliderLenta).animate({
+            left: -100 * (nowShowIndex + 1) + "%"
+        }, 600, function () {
+            sliderLenta.classList.remove('animated');
             bullets_draw(nowShowIndex)
-            })
-            if(sliderLenta.hasClass('animated')){
-                return;
-            }
+        })
+        if (sliderLenta.classList.contains('animated')) {
+            return;
         }
+    }
 };
 
 
-sliderBullets.onclick  = function() {
-    if(sliderLenta.hasClass('animated')){
+sliderBullets.onclick = function () {
+    if (sliderLenta.classList.contains('animated')) {
         return;
     }
-    sliderLenta.addClass('animated');
-    let bullet_index = sliderBullets.index(   $(this) );
-    
+    sliderLenta.classList.add('animated');
+    let bullet_index = sliderBullets.index($(this));
     bullets_draw(bullet_index);
-    sliderLenta.animate({
+    $(sliderLenta).animate({
         left: -100 * (nowShowIndex + 1) + "%"
-    }, 600, function(){
-        sliderLenta.removeClass('animated');
+    }, 600, function () {
+        sliderLenta.classList.remove('animated');
     })
 };
 
@@ -104,7 +102,7 @@ sliderBullets.onclick  = function() {
 
 
 
-function bullets_draw(show_index){
+function bullets_draw(show_index) {
     for (let i = 0; i < sliderBullets.length; i++) {
         sliderBullets[i].style.backgroundColor = "white";
     }
